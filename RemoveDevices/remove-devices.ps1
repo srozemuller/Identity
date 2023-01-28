24ab28267ba6e32ce71faf4570797f22f6be8d55
Sander Rozemuller <srozemuller@uniccloud.nl>
2023-01-28 08:50:01 +0100

    initial

---
+++ RemoveDevices/remove-devices.ps1
@@ -0,0 +1,44 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]$PathToCsv
+)
+
+try {
+    Install-Module MSAL.PS -Force
+    Import-Module -Name MSAL.PS
+} catch {
+    Write-Host "MSAL.PS not imported, $_"
+    exit
+}
+
+$clientId = "14d82eec-204b-4c2f-b7e8-296a70dab67e"
+$Scopes = "https://graph.microsoft.com/DeviceManagementManagedDevices.ReadWrite.All","https://graph.microsoft.com/Directory.ReadWrite.All","https://graph.microsoft.com/Directory.AccessAsUser.All"
+$token = Get-Msaltoken -ClientId $clientId -Scopes $Scopes
+$authHeader = @{
+    Authorization  = 'Bearer ' + $token.AccessToken
+}
+
+$csv = Import-Csv -Path $PathToCsv
+foreach ($row in $csv) {
+	try {
+		$deleteDeviceIntuneUrl = "https://graph.microsoft.com/beta/deviceManagement/managedDevices('{0}')" -f $row.Id
+		$deviceInfo = Invoke-RestMethod -Method Get -Uri $deleteDeviceIntuneUrl -Headers $AuthHeader
+	} catch {
+		Write-Host "Error getting device $($row.Id) from Intune"
+	}
+	try {
+		$getAadDeviceUrl = "https://graph.microsoft.com/beta/devices?`$filter=deviceId eq '{0}'" -f $deviceInfo.azureADDeviceId
+		$deviceAadInfo = Invoke-RestMethod -Method Get -Uri $getAadDeviceUrl -Headers $AuthHeader
+		$deleteAadDeviceUrl = "https://graph.microsoft.com/beta/devices/{0}" -f $deviceAadInfo.value.id
+	} catch {
+		Write-Host "Error getting device $($row.Id) from Azure AD"
+	}
+	try {
+		Invoke-RestMethod -Method Delete -Uri $deleteDeviceIntuneUrl -Headers $AuthHeader
+    	Invoke-RestMethod -Method Delete -Uri $deleteAadDeviceUrl -Headers $AuthHeader
+	}
+	catch {
+		Write-Host "Error deleting device $($row.Id), $_"
+	}
+}
